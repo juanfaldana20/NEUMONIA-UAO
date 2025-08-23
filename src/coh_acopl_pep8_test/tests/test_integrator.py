a1c9e7334c260fd34b219f18dab213d8d343c79a
Juan Fernando Aldana <juanfaldana2000@gmail.com>
2025-08-22 23:54:07 -0500

    Cambios en el readme

--- src/coh_acopl_pep8_test/tests/test_integrator.py
+++ src/coh_acopl_pep8_test/tests/test_integrator.py
@@ -13,7 +13,7 @@ def dummy_grad_cam(array):
 
 
 def test_predict_with_monkeypatch(monkeypatch):
-    # OJO: integrator importó model_fun y grad_cam en su namespace
+
     monkeypatch.setattr(integrator, "model_fun", lambda: DummyModel())
     monkeypatch.setattr(integrator, "grad_cam", dummy_grad_cam)
 
